48b8aa09bd125ee34654f12aa9e31e7b28c4ed9b
codedcoder <codedcoderrr@gmail.com>
2025-10-04 18:25:11 +0100

    auth

--- vite.config.ts
+++ vite.config.ts
@@ -13,6 +13,9 @@ if (
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  define: {
+    'import.meta.env.BACKEND_URL': JSON.stringify(process.env.BACKEND_URL)
+  },
   test: {
     environment: 'jsdom',
     globals: true,
